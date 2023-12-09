d19740740dc7cbc4b345d93249688e14d1ce23e4
MiaLiu123 <mialiu755@gmail.com>
2023-12-09 17:33:00 +0100

    slave encode decode changes

--- network/slave.py
+++ network/slave.py
@@ -6,6 +6,9 @@ from .constant import key_value as kv
 from alg import files
 from alg.seq import read_fna
 # from .master import Master
+import json
+import numpy as np
+import pickle
 
 class Slave(StrategyBase):
     def __init__(self, client):
@@ -16,7 +19,9 @@ class Slave(StrategyBase):
         self.last_heartbeat_time = time.time()
         self.master_addr = client.master_addr
         master_addr = self.client.addr_list[0]
-
+        with open('config.json', 'r') as f:
+            self.configs = json.load(f)
+        
         # 停止任务的标志
         self.master_timed_out = False
         self.stop_current_task = False
@@ -46,16 +51,26 @@ class Slave(StrategyBase):
                     "end_ind": 0,
                     "i_subvec": 0,
                     "subvec": [],
-                    "i_th_database": 0,
+                    "i_th_pattern": 0,
                 }
         '''
+        # test
+        # with open(self.configs['patterns'], 'r') as f:
+        #     content = f.read()
+        # print(f"Content: {content}")
+
 
         # 从文件系统读对应的sequence, pattern
-        i_th_database = data['i_th_database'] # 0, 1, 2, 3
-        sequence = read_fna(self.client.configs['database'][i_th_database], data['start_ind'], data['end_ind'])
-        pattern = read_fna(self.client.configs['patterns'], data['start_ind'], data['end_ind'])
+        i_th_pattern = data['i_th_pattern'] # 0, 1, 2, 3
+        sequence = read_fna(self.configs['database'], data['start_ind'], data['end_ind'])
+        print(f"sequence: {sequence}") # test
+
+        pattern = read_fna(self.configs['patterns'][i_th_pattern], 0, float('inf'))
+        print(f"pattern: {pattern}") # test
+
         N = len(sequence)
-        M = len(pattern)
+        M = len(pattern)    
+        print(N, M) # test
         
         # 计算第一个subvec的长度, 用于判断传给fillmatrix的pattern的长度
         if data['i_subvec'] == 0:
@@ -66,13 +81,26 @@ class Slave(StrategyBase):
         num_subvecs += remainder > 0
 
         #如果是第一块, upvec传空, 否则传上一块的最后一行
-        up_vec = [0 for _ in range(M)] if data['i_subvec'] == 0 else self.previous_bottom_vec
+        # up_vec = [0,0]
+        up_vec = [0 for _ in range(N)] if data['i_subvec'] == 0 else self.previous_bottom_vec
+        # print(len(up_vec)) 
+        
+
+        
 
         #for data['i_subvec'] in range(num_subvecs):
         if data['i_subvec'] < num_subvecs - 1:
             pattern_subvec = pattern[data['i_subvec'] * subvec_length : (data['i_subvec'] + 1) * (subvec_length-1)]
-            right_vec, bottom_vec, topK_dict = fill_matrix(pattern_subvec, sequence, up_vec, data['i_subvec'], data['start_ind'], data['end_ind'],self.client.K)
+            # print(f"pattern_subvec: {pattern_subvec}")
+            # print(f"sequence: {sequence}")
+            # print(f"up_vec: {up_vec}")
+            # print(f"data['i_subvec']: {data['i_subvec']}")
+            # print(f"data['start_ind']: {data['start_ind']}")
+            # print(f"data['end_ind']: {data['end_ind']}")
+            # print(f"self.client.K: {self.configs['k']}")
+            right_vec, bottom_vec, topK_dict = fill_matrix( data['subvec'], up_vec, data['i_subvec'], sequence, pattern_subvec, self.configs['k'])
             self.previous_bottom_vec = bottom_vec
+
             response_data = {
                 'start_ind': data['start_ind'],
                 'end_ind': data['end_ind'],
@@ -82,11 +110,12 @@ class Slave(StrategyBase):
                 'done': False
             }
             self.send_fillmatirx(response_data)
-
+            # test
+            print("response:" , response_data)
 
         elif data['i_subvec'] == num_subvecs - 1:
             pattern_subvec = pattern[data['i_subvec'] * subvec_length :]
-            right_vec, bottom_vec, topK_dict = fill_matrix(pattern_subvec, sequence, up_vec, data['i_subvec'], data['start_ind'], data['end_ind'],self.client.K)
+            right_vec, bottom_vec, topK_dict = fill_matrix( data['subvec'], up_vec, data['i_subvec'], sequence, pattern_subvec, self.configs['k'])
             response_data = {
                 'start_ind': data['start_ind'],
                 'end_ind': data['end_ind'],
@@ -95,29 +124,79 @@ class Slave(StrategyBase):
                 'topK': topK_dict,
                 'done': True
             }
-            self.send_fillmatirx(response_data)
+            # self.send_fillmatirx(response_data)
 
      
 
 
     def send_fillmatirx(self, data):
         # 将结果发送回 Master
-        self.client.send(self.master_addr, data)
+        data = pickle.dumps(data)
+        self.client.send(self.master_addr,data)
         print(f"Slave {self.rank} sends fillmatrix result to {self.master_addr}")
         
+
+    # trace_back(topK, start_s, end_s)
     def handle_traceback(self, data):
+        # 从Master接收到的数据
+        '''
+         data = {'type' : kv.T_TYPE,
+            'i_th_pattern' : i_th_pattern,
+            'topk_pos' : topk["pos"],
+            'topk_value' : topk["val"],
+            'start_ind' : topk["start_ind"],
+            'end_ind' : topk["end_ind"]}                       
+        '''
+        # 从文件系统读对应的sequence, pattern
+        i_th_pattern = data['i_th_pattern'] # 0, 1, 2, 3
+        sequence_path = self.configs['database']
+        print(f"sequence: {sequence_path}") # test
+
+        pattern_path = self.configs['patterns'][i_th_pattern]
+        print(f"pattern: {pattern_path}") # test
+
+        # N = len(sequence)
+        # M = len(pattern)    
+        # print(N, M) # test
+
+
+        # T_TYPE = "traceback"
+        # TOPK_VALUE = "topk_value"
+        # TOPK_POS = "topk_pos"
+
+       
+        
         # 执行 traceback 任务
-        result = trace_back(data['top_k_i'], data['x'], data['y'], data['start_ind'], data['end_ind'])
+        # trace_back(topK, start_s, end_s)
+        # 参数：
+        # topK - 一个字典 {“value":value,"i_subvec":i_subvec,"xy":(x,y)} #键值待统一       注：y需要从子块坐标变成整块的坐标
+        # start_s - K值所在seq子块的起始索引
+        # end_s - K值所在seq子块的结束索引
+
+        # 返回：
+        # aligned_p_s - pattern的alignment结果
+        # aligned_s_s - seq的alignment结果
+        topK = {
+            "value": data['topk_value'],
+            "i_subvec": 0, # TODO
+            "xy": data['topk_pos']
+        }
+        aligned_p_s, aligned_s_s = trace_back(topK, data['start_ind'], data['end_ind'], sequence_path, pattern_path)
         # 将结果发送回 Master
-        #TODO
+        response_data = {
+            'alignment': aligned_s_s,
+            'i_th_pattern': i_th_pattern,
+            'topk_pos': data['topk_pos'],
+            'topk_value': data['topk_value'],
+        }
+        print("tb_response_data:" , response_data)
+        self.send_traceback(response_data)
 
-        # top_k_i: value
-        # x:
-        # y:
-        # start_ind
-        # end_ind
-        pass
-
+    def send_traceback(self, data):
+        # 将结果发送回 Master
+        data = pickle.dumps(data)
+        self.client.send(self.master_addr,data)
+        print(f"Slave {self.rank} sends traceback result to {self.master_addr}")
 
 
     def iter(self):
@@ -125,30 +204,59 @@ class Slave(StrategyBase):
         # 检查是否收到master的心跳包，如果超时，则处理master超时
         # call alg to fill matrix, traceback
         # send to M
-        while True:
-            # 检查是否收到master的心跳包，如果超时，则处理master超时
-            self.check_if_master_alive(timeout=5)
-           # 处理任务队列中的任务
-            if not self.job_queue.empty():
-                task = self.job_queue.get()
+        # while True:
+        #     # 检查是否收到master的心跳包，如果超时，则处理master超时
+        #     self.check_if_master_alive(timeout=5)
+        #    # 处理任务队列中的任务
+        #     if not self.job_queue.empty():
+        #         task = self.job_queue.get()
 
-                if self.stop_current_task:
-                    print("Slave is stopping current task.")
-                    continue  # 跳过当前任务
+        #         if self.stop_current_task:
+        #             print("Slave is stopping current task.")
+        #             continue  # 跳过当前任务
 
-                if task['type'] == 'fillmatrix':
-                    self.handle_fillmatrix(task)
-                elif task['type'] == 'traceback':
-                    self.handle_traceback(task)
+        #         if task['type'] == 'fillmatrix':
+        #             self.handle_fillmatrix(task)
+        #         elif task['type'] == 'traceback':
+        #             self.handle_traceback(task)
 
-            time.sleep(0.1)  # 休眠0.1秒，避免CPU占用过高
+        #     time.sleep(0.1)  # 休眠0.1秒，避免CPU占用过高
+    # test
+        self.test_handle_fillmatrix()
+        # self.test_handle_traceback()
+        # pass
 
+    # test
+    def test_handle_fillmatrix(self):
 
+    # 创建一个 data 字典
+        data = {
+            "start_ind": 0,
+            "end_ind": 50,
+            "i_subvec": 0,
+            "subvec": [0,0,0],
+            "i_th_pattern": 0,
+        }
+
+        # 调用 handle_fillmatrix 方法
+        self.handle_fillmatrix(data)
+
+    def test_handle_traceback(self):
+        # 创建一个 data 字典
+        data = {
+            'i_th_pattern' : 0,
+            'topk_pos' : (1,2),
+            'topk_value' : 6,
+            'start_ind' : 0,
+            'end_ind' : 10}
+
+        # 调用 handle_traceback 方法
+        self.handle_traceback(data)
 
     #从master接收数据
     def recv(self, addr, data):
         if data:
-            data = data.decode()
+            data = pickle.loads(data)
             print("receive: ", data)
             if data == 'Heartbeat':
                 # 处理心跳包
@@ -159,16 +267,6 @@ class Slave(StrategyBase):
             #     self.handle_remake_command()
             else:
                 self.job_queue.put(data)
-            # elif data ['type']== 'remake':
-            #     # 处理从master收到的 remake 命令
-            #     self.job_queue.put(data)
-            # elif data['type'] == 'fillmatrix':
-            #     # 添加到工作队列
-            #     self.job_queue.put(data)
-            # elif data['type'] == 'traceback':
-            #     # 添加到 工作队列
-            #     self.job_queue.put(data)
-    
 
 
 
@@ -218,3 +316,5 @@ class Slave(StrategyBase):
 #         self.master_timed_out = False
 #         master_addr = self.client.addr_list[1]
 #         print("Reset complete. Ready to connect to the new Master.")
+
+
