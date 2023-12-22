efdf8fd4dc559080d29dd6722406311de52f9737
student <me@student.vu.nl>
2023-12-22 21:12:05 +0100

    add eval

--- das5.sh
+++ das5.sh
@@ -2,11 +2,30 @@
 
 rm ./output/*
 for file in $(find . -maxdepth 1 -type f \( -name "SlurmLog_*" -o -name ".nfs*" \)); do
-    echo "remove temporary log: $file"
+    # echo "remove temporary log: $file"
     rm "$file"
 done
 
 sbatch_output=$(sbatch job.slurm)
 job_id=$(echo $sbatch_output | grep -oP '(?<=Submitted batch job )\d+')
-log_file="SlurmLog_${job_id}.out"
-echo "Check file '$log_file' to debuuuuuuuuuuuug! This file is dynamically generated, do not modify it during the process! hahaha"
+
+while [ ! -f ./output/match.txt ]; do
+    sleep 1
+done
+
+sleep 5
+
+max_number=$(awk '{print $1}' ./output/match.txt | sort -nr | head -n 1)
+
+# you can change arguments by checking `alg.py`
+output=$(./ssw_test -c -m 3 -x 3 -o 2 -e 2 data/fna_db/covid_S.fna data/patterns/test.fna | grep 'optimal_alignment_score')
+alignment_score=$(echo "$output" | grep -oP '(?<=optimal_alignment_score: )\d+' | head -n 1)
+
+echo "Max number from match.txt: $max_number"
+echo "Alignment score from ssw_test: $alignment_score"
+
+if [ "$alignment_score" -eq "$max_number" ]; then
+    echo "The optimal alignment score ($alignment_score) is equal to the maximum number ($max_number) in match.txt."
+else
+    echo "The optimal alignment score ($alignment_score) is not equal to the maximum number ($max_number) in match.txt."
+fi
